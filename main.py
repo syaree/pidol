ce9e546f5f27fcd97650586ee0df5f2e644e1714
JustSImple <fulan.gulan@gmail.com>
2020-10-14 00:18:25 +0700

    buat window dengan class based

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import gi
 
 gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class App(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Coba GTK")
+
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_btn_click)
+
+        self.add(self.button)
+
+    def on_btn_click(self, widget):
+        print("Uji coba menggunakan GTK")
 
 
 if __name__ == "__main__":
-    from gi.repository import Gtk
-
-    window = Gtk.Window(title="Coba GTK")
-    window.show()
-
+    window = App()
     window.connect("destroy", Gtk.main_quit)
+    window.show_all()
+
     Gtk.main()
