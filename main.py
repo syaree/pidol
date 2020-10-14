2c435fbadd2bfa98817526ded1fb2a5e7c52ec7e
syariefa <syareeefa@gmail.com>
2020-10-14 16:23:23 +0700

    memuat ui dari file

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import gi
-
 gi.require_version("Gtk", "3.0")
+
 from gi.repository import Gtk
 
 
-class App(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Coba GTK")
+class Handler:
+    def onDestroy(self, _):
+        Gtk.main_quit()
 
-        self.button = Gtk.Button(label="Click Here")
-        self.button.connect("clicked", self.on_btn_click)
-
-        self.add(self.button)
-
-    def on_btn_click(self, widget):
-        print("Uji coba menggunakan GTK")
+    def onButtonPressed(self, _):
+        print("Clicked!")
 
 
 if __name__ == "__main__":
-    window = App()
-    window.connect("destroy", Gtk.main_quit)
+    builder = Gtk.Builder()
+    builder.add_from_file("ui.glade")
+    builder.connect_signals(Handler())
+
+    window = builder.get_object("window1")
     window.show_all()
 
     Gtk.main()
