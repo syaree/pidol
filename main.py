faeba7926c8031ddf699b924dbdb1c2845554662
syariefa <syareeefa@gmail.com>
2020-10-15 14:02:56 +0700

    letakin toolbar

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow,
+    QLabel, QToolBar, QAction,
+    QStatusBar, QCheckBox
+)
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt, QSize
 
 
 class MainWindow(QMainWindow):
@@ -9,12 +14,38 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("PIDOL")
         self.setMinimumSize(400, 250)
+        self.windowTitleChanged.connect(lambda x: print(x))
 
         label = QLabel()
         label.setText("Selamat Datang")
         label.setAlignment(Qt.AlignCenter)
 
         self.setCentralWidget(label)
+
+        toolbar = QToolBar("Toolbar")
+        toolbar.setIconSize(QSize(16, 16))
+        toolbar.setContextMenuPolicy(Qt.PreventContextMenu)
+
+        self.addToolBar(toolbar)
+
+        btn_action = QAction(QIcon("asset/floppy.png"), "Aksi", self)
+        btn_action.setStatusTip("Status Aksi")
+        btn_action.setCheckable(True)
+        btn_action.triggered.connect(lambda text: print("Triggered", text))
+        toolbar.addAction(btn_action)
+
+        toolbar.addSeparator()
+
+        btn_action2 = QAction(QIcon("asset/floppy.png"), "Aksi 2", self)
+        btn_action2.setStatusTip("Status Aksi 2")
+        btn_action2.setCheckable(True)
+        btn_action2.triggered.connect(lambda text: print("Triggered 2", text))
+        toolbar.addAction(btn_action2)
+
+        toolbar.addWidget(QLabel("Aktivasi"))
+        toolbar.addWidget(QCheckBox())
+
+        self.setStatusBar(QStatusBar(self))
 
 
 if __name__ == "__main__":
