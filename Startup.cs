150d98ae7324724070cf5725963565a698edc609
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-10-18 15:13:26 +0700

    atur ulang posisi properti

--- Startup.cs
+++ Startup.cs
@@ -15,12 +15,12 @@ namespace pidol
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -36,7 +36,7 @@ namespace pidol
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
