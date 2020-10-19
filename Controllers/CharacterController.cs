98158c91e244f44b1c860464fad976f82c2b2376
syariefa <syareeefa@gmail.com>
2020-10-19 09:49:26 +0700

    buat get methods

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 using pidol.Models;
@@ -8,10 +10,21 @@ namespace pidol.Controllers
     [Route("[Controller]")]
     public class CharacterController: ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> _characters = new List<Character> {
+            new Character { Id = 1 },
+            new Character { Id = 2, Name = "Sam" }
+        };
 
+        [HttpGet]
         public IActionResult Get() {
-            return Ok(knight);
+            return Ok(_characters);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id) {
+            Character selected_character = _characters.FirstOrDefault(chr => chr.Id.Equals(id));
+
+            return Ok(selected_character);
         }
     }
 }
