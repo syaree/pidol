77ec2314c7c0c3e4279af3ca35268a53bb0864b8
syariefa <syareeefa@gmail.com>
2020-10-21 14:06:56 +0700

    buat auto mapper profile

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using pidol.Dtos.Character;
+using pidol.Models;
+
+namespace pidol
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character, GetCharacterDto>();
+            CreateMap<AddCharacterDto, GetCharacterDto>();
+        }
+    }
+}
